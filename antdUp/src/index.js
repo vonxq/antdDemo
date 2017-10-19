0b52baac5de9152a19ae2d75ed2b7b058049608a
anne:ws2012 <vonxq@foxmail.com>
2017-10-19 11:21:35 +0800

    init

--- antdUp/src/index.js
+++ antdUp/src/index.js
@@ -5,7 +5,7 @@ const TabPane = Tabs.TabPane
 import './index.css'
 const Ha = () => (
   <div className="App">
-    <Tabs defaultActiveKey="1" type="card">
+    <Tabs type="editable-card"defaultActiveKey="1" type="card">
         <TabPane tab="标签页1" key="1">content of tabpane1</TabPane>
         <TabPane tab="标签页2" key="2">content of tabpane2</TabPane>
         <TabPane tab="标签页3" key="3">
